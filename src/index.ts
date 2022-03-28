dad3cbd2678e8e4fd7032a94354959b40ff0500f
nicolae.topala <nicolae.topala13@gmail.com>
2022-03-28 13:17:08 +0300

    Setup for CRUD

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,31 @@ import { graphqlHTTP } from "express-graphql";
 import cors from "cors";
 import { DataSource } from "typeorm";
 
+import { schema } from "./Schema";
+
 const main = async () => {
+  let dataSource = new DataSource({
+    type: "mysql",
+    database: "ums_db",
+    username: "root",
+    password: "123321",
+    logging: true,
+    synchronize: true,
+    entities: [],
+  });
+
+  let connection = await dataSource.initialize();
+
   const app = express();
   app.use(cors());
   app.use(express.json());
-  /* app.use("/graphql", graphqlHTTP({
-        schema,
-        graphiql: true
-    })); */
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema,
+      graphiql: true,
+    })
+  );
 
   app.listen(3001, () => {
     console.log("Server running on port 3001");
