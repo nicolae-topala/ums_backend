5e51e0b389181b7c7d4bca57d2d1dc8a4bb54d8e
nicolae.topala <nicolae.topala13@gmail.com>
2022-04-01 23:54:32 +0300

    removed GraphQL and Config dependencies

--- src/Schema/TypeDefs/Message.ts
+++
@@ -1,9 +0,0 @@
-import { GraphQLObjectType, GraphQLString, GraphQLBoolean } from "graphql";
-
-export const MessageType = new GraphQLObjectType({
-  name: "Message",
-  fields: () => ({
-    successful: { type: GraphQLBoolean },
-    message: { type: GraphQLString },
-  }),
-});
